5d8aad1891e5118e3d274f1c4e31f0aa0b892e4d
Joseph Oluwayemi <joejoeoyemi@gmail.com>
2022-10-29 19:27:16 -0700

    Edited files again

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -9,8 +9,12 @@
   */
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0 || year % 400 == 0) && (year % 100 == 0))
+	if ((year % 4 == 0 || year % 400 == 0) || (year % 100 == 0))
 	{
+		if (month >= 2 && day >= 60)
+		{
+			day++;
+		}
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
@@ -22,7 +26,7 @@ void print_remaining_days(int month, int day, int year)
 		}
 		else
 		{
-			printf("Day of the year: %d\n", ++day);
+			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
