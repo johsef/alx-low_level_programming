3cb225c714586f0e5bda3ef3071795771403104b
Joseph Oluwayemi <joejoeoyemi@gmail.com>
2022-10-31 19:12:10 -0700

    Edited Fizz Buzz file

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,14 +12,15 @@ int main(void)
 	while (i <= 100)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz ");
+			printf("%s ", "FizzBuzz");
 		else if (i % 3 == 0)
-			printf("Fizz ");
+			printf("%s ", "Fizz");
 		else if (i % 5 == 0)
-			printf("Buzz ");
+			printf("%s ", "Buzz");
 		else
 			printf("%d ", i);
 		i++;
 	}
+	putchar('\n');
 	return (0);
 }
