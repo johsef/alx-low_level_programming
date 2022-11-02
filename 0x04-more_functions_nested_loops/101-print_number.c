ea41f179e1f0f7d22bd48d5d19b5cec13dde3083
Joseph Oluwayemi <joejoeoyemi@gmail.com>
2022-11-02 10:09:06 -0700

    101-print_number file

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,18 +1,44 @@
 #include "main.h"
 /**
-   * print_number - A function that prints an integer.
-    * @n:  integer number
-     * Return: Prints number to terminal.
-      */
+   * print_number -  A function that prints an integer.
+    * @n: integer
+     * Return: Nothing
+ */
+void print_number(int n)
+{
+	/**
+	 *Handles negative number
+	 *  and overflow
+	 */
+	int last_digit, rev, ch;
 
- void print_number(int n)
-	 {
-		 if (n < 0)
-		 {
-			 putchar('-');
-			 n = -n;   
-		 }
-		 if (n / 10)
-			 print_number(n/10);       
-		 putchar(n % 10 + '0');
-	 }
+	if (n < 0)
+	{
+		_putchar('-');
+		last_digit = ('0' - n % 10);
+		n = n / -10;
+	}
+	else
+	{
+		last_digit = ('0' + n % 10);
+		n = n / 10;
+	}
+	/*Reverse number*/
+	rev = 0;
+	while (n > 0)
+	{
+		rev = (rev * 10) + (n % 10);
+		n = n / 10;
+	}
+	/*Outputs reversed number*/
+	while (rev > 0)
+	{
+		ch = rev % 10 + '0';
+		rev = rev / 10;
+		_putchar(ch);
+	}
+	_putchar(last_digit);
+}
+
+
+
