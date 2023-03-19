22f6e56bccde114e58140ea25b2882bba86c5556
Joseph Oluwayemi <joejoeoyemi@gmail.com>
2023-03-19 02:41:47 -0700

    *

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -16,10 +16,10 @@ void print_array(int *a, int n)
 	{
 		if (i == n - 1)
 		{
-			printf(" %d\n", a[i]);
+			printf("%d\n", a[i]);
 			break;
 		}
-		printf(" %d, ", a[i]);
+		printf("%d, ", a[i]);
 		i++;
 	}
 }
