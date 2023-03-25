da1027463f64197fdef361b2138e054189c6083e
Joseph Oluwayemi <joejoeoyemi@gmail.com>
2023-03-25 09:21:45 -0700

    *

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,5 +1,29 @@
 #include "main.h"
 /**
+ * find_prime - a function that finds prime of a number
+ * @a: number to find prime
+ * @t: test value
+ * Return: prime number
+ */
+int find_prime(int a, int t)
+{
+	if (t <= 1)
+		return (0);
+	if (t == 2)
+		return (1);
+	if (a != t)
+	{
+		if ((t % a) != 0)
+		{
+			return (1);
+			find_prime(a + 1, t);
+		}
+		else
+			return (0);
+	}
+	return (0);
+}
+/**
  * is_prime_number -  a function that returns 1 if the
  * input integer is a prime number, otherwise return 0.
  * @n: number
