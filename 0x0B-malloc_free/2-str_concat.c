99d9859666cfd0d91d5f552ec4eba557a5821c73
Joseph Oluwayemi <joejoeoyemi@gmail.com>
2023-03-25 11:17:32 -0700

    *

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,6 +12,10 @@ char *str_concat(char *s1, char *s2)
 	int len1, len2, len, i, j;
 	char *st;
 
+	if (s1 == NULL)
+		s1 = " ";
+	if (s2 == NULL)
+		s2 = " ";
 	len1 = strlen(s1);
 	len2 = strlen(s2);
 	len = len1 + len2;
