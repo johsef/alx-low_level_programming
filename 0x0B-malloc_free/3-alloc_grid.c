c2032e88730dc045b3099c88abf57c9a988eeeb7
Joseph Oluwayemi <joejoeoyemi@gmail.com>
2023-03-25 11:56:12 -0700

    *

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,7 +18,7 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	grid = malloc(height * sizeof(int *));
 	if (grid == NULL)
-		return (NULL);	
+		return (NULL);
 	for (i = 0; i < height; i++)
 	{
 		grid[i] = malloc(width * sizeof(int));
