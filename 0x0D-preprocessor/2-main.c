b07e2e5e7da6fde134e3652e442e0a29fd49b423
Joseph Oluwayemi <joejoeoyemi@gmail.com>
2023-03-28 05:31:29 -0700

    Added other files

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+  * main - Entry of code
+  * Return: Nothing
+  */
+int main(void)
+{
+	printf("%s", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,5 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+#define ABS(x) (x < 1 ? x : x)
+typedef int make_iso_compilers_happy;
+#endif /*END FUNCTION_LIKE_MACRO_H*/
